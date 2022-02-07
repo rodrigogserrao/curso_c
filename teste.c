876e26515f420f2cbafe67adfce167639d18a877
rodrigogserrao@uol.com.br <rodrigogserrao@uol.com.br>
2022-02-07 17:17:21 -0300

    inclusão de total

--- teste.c
+++ teste.c
@@ -9,14 +9,18 @@ int main()
     if(salario <= 1000.00)
     {
         printf("Seu salario \x82 R$ %.2f, assim vai ter bonus de 10%% = %.2f\n", salario, total = (salario*10)/100);
+        printf("Assim seu salario total \x82 R$ %.2f\n", salario + total);
+
     }
     else if(salario > 1000.00 && salario <= 5000.00)
     {
         printf("Seu salario \x82 R$ %.2f, assim vai ter bonus de 20%% = %.2f\n", salario, total = (salario*20)/100);
+        printf("Assim seu salario total \x82 R$ %.2f\n", salario + total);
     }
     else // if(salario > 5000.00)
     {
         printf("Seu salario \x82 R$ %.2f, assim vai ter bonus de 30%% = %.2f\n", salario, total = (salario*30)/100);
+        printf("Assim seu salario total \x82 R$ %.2f\n", salario + total);
     }
 
     system("PAUSE");
